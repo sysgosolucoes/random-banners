641d53be71ad2e0ff61cefcd7092e1037a3dbc9c
Bruno Barreto <barretowiisk@gmail.com>
2026-04-14 12:14:22 -0300

    Atualizando banners: substituindo imagem do banner do Dia do Trabalhador e ajustando URLs, datas e status

--- functions/ads.js
+++ functions/ads.js
@@ -1,32 +1,32 @@
 export const ads = [
     {
         id: 1,
-        image: "/images/imagem1.jpg",
-        url: "https://cliente1.com",
+        image: "/images/imagem1.png",
+        url: "https://sysgo.com.br/",
         weight: 1,                // quanto maior, mais aparece
         priority: 1,              // 1 = normal, 2 = destaque
         active: true,
         startDate: "2026-01-01",
-        endDate: "2026-12-31"
+        endDate: "3000-01-01"
     },
     {
         id: 2,
         image: "/images/imagem2.jpg",
-        url: "https://cliente2.com",
+        url: "https://sysgo.com.br/",
         weight: 1,
         priority: 1,
-        active: true,
+        active: false,
         startDate: "2026-01-01",
-        endDate: "2026-06-30"
+        endDate: "3000-01-01"
     },
     {
-        id: 3,
+        id: 50,
         image: "/images/DiaDoTrabalhador.png",
         url: "https://sysgo.com.br/",
         weight: 5,
         priority: 2,
         active: true,
-        startDate: "2026-04-14",
-        endDate: "2026-04-15"
+        startDate: "2026-04-27",
+        endDate: "2026-05-04"
     }
 ];
